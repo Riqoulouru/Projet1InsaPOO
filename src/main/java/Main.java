652656644a7e29ff1ee1be9ffd72081b1623eb21
Barbary-Theo <theo.barbary25@gmail.com>
2021-10-08 16:32:10 +0200

    Adding login part and modify getSavesClient for returning ArrayList instead of table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 /***
@@ -10,23 +11,58 @@ import java.util.Scanner;
  *      Comprennant : les différents aliments / plats / Boisson ... Et les clients.
  */
 public class Main {
+    public static Client clientConnected = null;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 //        InitiliazeAllElements();
 
-
+        ArrayList<String> idClients = getSavesClient();
         Scanner sc = new Scanner(System.in);
-        boolean login = false;
 
-        while (!login){
-            System.out.println("Veuillez saisir votre id:");
+        System.out.println("""
+                Que voulez vous faire :
+                1 : Se connecter
+                2 : Créer un id""");
 
+        int connection = Integer.parseInt(sc.nextLine());
+        while (connection != 1 && connection != 2){
+            connection = Integer.parseInt(sc.nextLine());
         }
 
+        //En sortant du switch le client est obligatoirement connecté
+        switch (connection){
+            // S'il se connecte
+            case 1 -> {
+                System.out.print("Indiquer votre id : ");
+                String inputId = sc.nextLine();
+                //Si l'id n'existe pas alors création de celui-ci
+                if(!idClients.contains(inputId)){
 
+                    System.out.println("Id non existant, création de cet id ...");
+                    System.out.print("Indiquer votre nom/prénom sout la forme : Nom/Prénom : ");
+                    String NameFirstName = sc.nextLine();
+                    String[] newClientName = NameFirstName.split("/");
+                    clientConnected = new Client(Integer.parseInt(idClients.get(idClients.size() - 1)) + 1, newClientName[0], newClientName[1]);
+                }
+                //Sinon le connecter
+                else {
+                    clientConnected = Client.getClientById(Integer.parseInt(inputId));
+                }
+            }
+            //S'il crée un compte Client
+            case 2 -> {
+                //Récupérer le dernier id et lui donner un nom/prénom
+                int newIdClient = Integer.parseInt(idClients.get(idClients.size() - 1)) + 1;
+                System.out.print("Votre id sera "+ newIdClient +", indiquer votre nom/prénom sout la forme : Nom/Prénom : ");
+                String NameFirstName = sc.nextLine();
+                String[] newClientName = NameFirstName.split("/");
+                clientConnected = new Client(newIdClient, newClientName[0], newClientName[1]);
 
+            }
+        }
+        sc.close();
 
-
+        System.out.println(clientConnected.getNom());
 
     }
 
@@ -79,6 +115,35 @@ public class Main {
         rizoto.saveItem();
         cl.saveItem();
 
+    }
+
+    public static ArrayList<String> getSavesClient(){
+        File dossier=new File("Save\\Client\\");
+        File[] liste_saves=dossier.listFiles();
+        assert liste_saves != null;
+
+        boolean exist = Objects.requireNonNull(liste_saves).length!=0; // Vérfier que le dossier n'est pas vide
+
+        ArrayList<String> liste_string = new ArrayList<>();
+        if (exist) {
+            int n = 0;
+            for (File file : liste_saves) { //Pour chaque sauvegardes présente, on ajoute son nom dans le tableau de retour
+
+                liste_string.add(getRealName(file.getName())); //On récupère le nom des fichiers de sauvegarde
+                n++;
+            }
+        }
+        return liste_string; //On retourne la liste contenant les noms de chaques saves au format string
+    }
+
+    public static String getRealName(String original_name){
+        String file_name = "";
+        int i = 0;
+        while(original_name.charAt(i) != '.'){
+            file_name += original_name.charAt(i);
+            i++;
+        }
+        return file_name;
     }
 
 }
